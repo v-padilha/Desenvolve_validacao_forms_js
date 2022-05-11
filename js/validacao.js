873f0a1cd82c22674558ec51897a272c63d93841
Vinycius Osadczuk Padilha <vinyosadczuk@hotmail.com>
2022-05-11 16:15:07 -0300

    Começa a desenha validação de CPF

--- js/validacao.js
+++ js/validacao.js
@@ -35,6 +35,10 @@ const mensagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.'
     }
 
 
@@ -42,6 +46,7 @@ const mensagensDeErro = {
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
+    cpf:input => validaCPF(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -70,3 +75,54 @@ function maiorQue18(data) {
 
     return dataMais18 <= dataAtual
 }
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if (!checaCPFRepetido(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if (valor == cpf) {
+                cpfValido = false
+        }
+    })
+
+    return cpfValido
+}
+
+function checaEstruturaCPF(cpf) {
+
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function confirmaDigito(soma) {
+    return 11-(soma % 11)
+}
+
+
+//let soma = (10*1) + (9*2) + (8*3)...(2*9)
+
+//let digitoVerificador = 11-(soma % 11)
